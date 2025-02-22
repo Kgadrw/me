e12f5bf0eca472bbf9d7da3a58b60143db974a14
GAD KALISA <kalisagad05@gmail.com>
2025-02-22 21:24:42 +0200

    Initial commit

---
+++ src/pages/Landingpage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import AboutUs from "../components/AboutUs";
+import Footer from "../components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <AboutUs />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
