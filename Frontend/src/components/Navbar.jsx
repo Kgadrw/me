79fb4d68ab6df187e466fa82c19592fa168257c0
Kgadrw <kalisagad05@gmail.com>
2025-03-14 15:09:48 +0200

    used link

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Menu, X } from "lucide-react"; // Menu and X icons
-import { Github, Twitter, Linkedin } from "lucide-react"; // Correct imports for social icons
+import { Link } from "react-router-dom";
+import { Menu, X, Github, Twitter, Linkedin } from "lucide-react";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -34,23 +34,23 @@ const Navbar = () => {
       <div className="flex items-center justify-between">
         {/* Left Side: Logo with Text */}
         <div className="flex items-center space-x-2 pl-20">
-          <a
-            href="/"
+          <Link
+            to="/"
             className="text-3xl sm:text-4xl font-extrabold text-blue-600"
           >
             <img src="/gad.png" alt="logo" className="h-8  sm:h-10" />
-          </a>
+          </Link>
         </div>
 
         {/* Right Side: Blog Link, Social Media Icons, and Mobile Menu Button */}
         <div className="flex items-center space-x-4 sm:space-x-6">
           {/* Blog Link */}
-          <a
-            href="/about"
+          <Link
+            to="/about"
             className="text-sm sm:text-base font-semibold text-black hover:text-blue-600 hidden md:block"
           >
             Blogs
-          </a>
+          </Link>
 
           {/* Social Media Icons with Circle Border */}
           <div className="flex space-x-4">
@@ -93,12 +93,12 @@ const Navbar = () => {
           className="md:hidden absolute top-0 left-0 right-0 bg-white px-8 py-6 mt-16 shadow-lg"
         >
           <div className="flex flex-col space-y-4">
-            <a
-              href="/about"
+            <Link
+              to="/about"
               className="text-black text-lg font-medium hover:text-blue-600"
             >
               Blogs
-            </a>
+            </Link>
           </div>
         </div>
       )}
